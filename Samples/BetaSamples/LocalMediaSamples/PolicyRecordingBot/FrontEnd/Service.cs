2f77f52d2c8a5917a90fe453a876839772759442
kostas-kakoutopoulos <konstantinos.kakoutopoulos@teracloud.com>
2026-05-04 17:37:55 +0300

    style fixes

--- Samples/BetaSamples/LocalMediaSamples/PolicyRecordingBot/FrontEnd/Service.cs
+++ Samples/BetaSamples/LocalMediaSamples/PolicyRecordingBot/FrontEnd/Service.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Service.cs" company="Microsoft Corporation">
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
